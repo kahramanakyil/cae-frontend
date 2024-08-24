6f7529dd95b31575de3c5cad056cab6c821b0612
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2024-08-24 16:36:52 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     devServer: {
         proxy: {
-            "/v1/consume_cpu": {
-                target: process.env.VUE_APP_URL,
+            '/v1/consume_cpu': {
+                target: 'app-test-backend:9090',
                 changeOrigin: true,
                 ws: true
             }
